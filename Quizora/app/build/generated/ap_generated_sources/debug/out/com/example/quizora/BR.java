a6bcf9f9b1bf590adece5069804904fd4c84bb68
Paul Justine <pato.rimando.up@phinmaed.com>
2025-01-29 09:08:14 +0800

    Test Update for up commiting

---
+++ Quizora/app/build/generated/ap_generated_sources/debug/out/com/example/quizora/BR.java
@@ -0,0 +1,5 @@
+package com.example.quizora;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ Quizora/app/build/generated/data_binding_trigger/debug/com/example/quizora/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.quizora;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
